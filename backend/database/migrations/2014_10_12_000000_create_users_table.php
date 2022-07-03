89ad016b6f4188c5326ddb742fb56e017b751b68
vladimir <vladimirmawla2@gmail.com>
2022-07-04 01:47:47 +0300

    Filling users table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('user_type');
+            $table->string('profile_img');
             $table->string('password');
+            $table->integer('points');
+            $table->string('job_title');
+            $table->string('company_id');
             $table->rememberToken();
             $table->timestamps();
         });
