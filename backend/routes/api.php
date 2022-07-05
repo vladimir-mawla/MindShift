8f31a05620bbcc56caf7ac0eb4f1119c724ac294
vladimir <vladimirmawla2@gmail.com>
2022-07-05 22:12:20 +0300

    Creating route for add game api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\GameController;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+Route::group(['prefix' => 'v1'], function(){
+
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+
+    });
+
+    Route::controller(GameController::class)->group(['prefix' => 'games'], function(){
+        Route::post('add_game', 'addGame');
+
+    });
 
 });
