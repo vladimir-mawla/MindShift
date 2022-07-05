a9905cddd8b10336a1c755b20214b4cabfec72bb
vladimir <vladimirmawla2@gmail.com>
2022-07-05 21:20:25 +0300

    Creating add game api

--- backend/app/Http/Controllers/GameController.php
+++ backend/app/Http/Controllers/GameController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+    public function addGame(Request $request){
+
+        $game = new Game;
+        $game->name = $request->name;
+        $game->description = $request->description;
+        $game->points = $request->points;
+        $game->img = $request->img;
+        $game->save();
+
+        return response()->json([
+            "status" => "Success",
+            "game" => $game,
+        ], 200);
+    }
 }
