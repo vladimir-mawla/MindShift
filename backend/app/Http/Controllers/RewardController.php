d13c9f8fa3377ee4f823f7219560bf60b4a219e9
vladimir <vladimirmawla2@gmail.com>
2022-07-07 19:19:18 +0300

    Creating add reward api

--- backend/app/Http/Controllers/RewardController.php
+++ backend/app/Http/Controllers/RewardController.php
@@ -7,5 +7,17 @@ use App\Models\Reward;
 
 class RewardController extends Controller
 {
-    //
+    //Add Reward API
+    public function addReward(Request $request){
+        $reward = new Reward;
+        $reward->name = $request->name;
+        $reward->description = $request->description;
+        $reward->needed_points = $request->needed_points;
+        $reward->save();
+
+        return response()->json([
+            "status" => "Success",
+            "reward" => $reward,
+        ], 200);
+    }
 }
