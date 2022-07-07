002276a6b9f88bebf6f359d5e2ce544a5f741ddf
vladimir <vladimirmawla2@gmail.com>
2022-07-07 23:13:24 +0300

    Creating add member api

--- backend/app/Http/Controllers/MemberController.php
+++ backend/app/Http/Controllers/MemberController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class MemebrController extends Controller
 {
-    //
+        // Add Member API
+        public function addMember(Request $request){
+
+            $member = new Member;
+            $member->user_id = $request->user_id;
+            $member->group_id = $request->group_id;
+            $member->save();
+    
+            return response()->json([
+                "status" => "Success",
+                "member" => $member,
+            ], 200);
+        }
 }
