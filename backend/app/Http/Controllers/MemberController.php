8cbcea0bf1bd05406dea9c0bdcac2cee61857d4d
vladimir <vladimirmawla2@gmail.com>
2022-07-07 23:14:34 +0300

    Creating remove member api

--- backend/app/Http/Controllers/MemberController.php
+++ backend/app/Http/Controllers/MemberController.php
@@ -6,17 +6,27 @@ use Illuminate\Http\Request;
 
 class MemebrController extends Controller
 {
-        // Add Member API
-        public function addMember(Request $request){
+    // Add Member API
+    public function addMember(Request $request){
 
-            $member = new Member;
-            $member->user_id = $request->user_id;
-            $member->group_id = $request->group_id;
-            $member->save();
-    
-            return response()->json([
-                "status" => "Success",
-                "member" => $member,
-            ], 200);
-        }
+        $member = new Member;
+        $member->user_id = $request->user_id;
+        $member->group_id = $request->group_id;
+        $member->save();
+
+        return response()->json([
+            "status" => "Success",
+            "member" => $member,
+        ], 200);
+    }
+
+    // Remove Member API
+    public function removeMember(Request $request){
+        Member::where('id',$request->member_id)->delete();
+
+        return response()->json([
+            "Successfully Deleted",
+        ], 200);
+    }
+        
 }
