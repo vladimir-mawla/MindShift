44f2013317e0f715a9c8183b5f43ee49ec32ac2e
vladimir <vladimirmawla2@gmail.com>
2022-07-08 02:28:48 +0300

    Creating add achievements api

--- backend/app/Http/Controllers/AchievementController.php
+++ backend/app/Http/Controllers/AchievementController.php
@@ -8,5 +8,18 @@ use App\Models\Achievement;
 
 class AchievmentController extends Controller
 {
-    //
+    // Add Achievement API
+    public function addAchievement(Request $request){
+
+        $achievement = new Achievement;
+        $achievement->name = $request->name;
+        $achievement->points = $request->points;
+        $achievement->img = $request->img;
+        $achievement->save();
+
+        return response()->json([
+            "status" => "Success",
+            "achievement" => $achievement,
+        ], 200);
+    }
 }
