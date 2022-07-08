22915e9e60c64fba71ac96a76cd4765c736807b5
vladimir <vladimirmawla2@gmail.com>
2022-07-09 01:58:48 +0300

    Returning the login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,6 +3,28 @@ import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    
+  return (
+    <center>
+      <div className="form">
+        <input type="email" placeholder="email" id="login_email" />
+        <input
+          type="password"
+          placeholder="password"
+          id="login_password"
+          autoComplete="on"
+        />
+        <Button
+          text={"Login"}
+          className={"login-btn"}
+          onClick={() => {
+            ;
+          }}
+        />
+        <p className="goto-link">
+          <Link to={"/signup"}>Create Account</Link>
+        </p>
+      </div>
+    </center>
+  );
 };
 export default Login;
