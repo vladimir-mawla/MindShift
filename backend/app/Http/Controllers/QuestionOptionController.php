0231f8cd8cc2e0edbc22cea79b80af04f694f013
vladimir <vladimirmawla2@gmail.com>
2022-07-08 13:20:03 +0300

    Creating add question option api

--- backend/app/Http/Controllers/QuestionOptionController.php
+++ backend/app/Http/Controllers/QuestionOptionController.php
@@ -7,5 +7,17 @@ use App\Models\QuestionOption;
 
 class QuestionOptionController extends Controller
 {
-    //
+    //Add Question Option API
+    public function addQuestionOption(Request $request){
+
+        $question_option = new QuestionOption;
+        $question_option->question_id = $request->question_id;
+        $question_option->option = $request->option;
+
+        $question_option->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
