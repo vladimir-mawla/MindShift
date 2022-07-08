e576963764587ebdd9f42c20ee8393095d51cd63
vladimir <vladimirmawla2@gmail.com>
2022-07-08 13:40:10 +0300

    Creating add question type api

--- backend/app/Http/Controllers/QuestionTypeController.php
+++ backend/app/Http/Controllers/QuestionTypeController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\QuestionType;
 
 class QuestionTypeController extends Controller
 {
-    //
+    //Add Question Type API
+    public function addQuestionType(Request $request){
+
+        $question_type = new QuestionType;
+        $question_type->question_id = $request->question_id;
+        $question_type->type = $request->type;
+        $question_type->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
