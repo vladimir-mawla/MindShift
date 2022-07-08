33422bba58c9e7d0f25d61fb3eec19b88540beca
vladimir <vladimirmawla2@gmail.com>
2022-07-08 19:54:03 +0300

    Importing route from react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+
       </Routes>
     </BrowserRouter>
     );
