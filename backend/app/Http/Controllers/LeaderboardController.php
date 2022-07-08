45ca39c81565b8ee1c2895328c0c31d7f2df8a3b
vladimir <vladimirmawla2@gmail.com>
2022-07-08 13:54:12 +0300

    Creating add user to leaderboards table

--- backend/app/Http/Controllers/LeaderboardController.php
+++ backend/app/Http/Controllers/LeaderboardController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Leaderboard;
 
 class LeaderboardController extends Controller
 {
-    //
+    //Add Users API
+    public function addUser(Request $request){
+        $leaderboard = new Leaderboard;
+        $leaderboard->user_id = $request->user_id;
+        $leaderboard->save();
+
+        return response()->json([
+            "status" => "Success",
+            "leaderboard" => $leaderboard,
+        ], 200);
+    }
 }
