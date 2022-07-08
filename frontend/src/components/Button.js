e0c5bd8e9e72f8d0420919432342df5fb7956f07
vladimir <vladimirmawla2@gmail.com>
2022-07-08 19:25:30 +0300

    Creating button component

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ className, text, onClick, id }) => {
+  return (
+    <button className={className} onClick={onClick} id={id}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
