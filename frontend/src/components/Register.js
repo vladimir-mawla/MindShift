76dcef35d53f14b53e3476640dcc715b57b45d4d
vladimir <vladimirmawla2@gmail.com>
2022-07-09 17:54:36 +0300

    Creating return for the register component

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -7,5 +7,31 @@ import axios from "axios";
 
 const Register = () => {
 
+
+  return (
+    <center>
+      <Navbar />
+      <div className="form">
+        <input type="text" placeholder="name" ref={name} />
+        <input type="email" placeholder="email" ref={email} />
+        <input
+          type="password"
+          placeholder="password"
+          ref={password}
+          autoComplete="on"
+        />
+        <Button
+          text={"Login"}
+          className={"login-btn"}
+          onClick={() => {
+            register();
+          }}
+        />
+        <p className="goto-link">
+          <Link to={"/signup"}>Create Account</Link>
+        </p>
+      </div>
+    </center>
+  );
 };
 export default Register;
