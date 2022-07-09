46604712c996c6bf8a339af2c12cc6ed4c1cdae3
vladimir <vladimirmawla2@gmail.com>
2022-07-09 20:58:11 +0300

    Initializing landing page component

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useNavigate, Link } from "react-router-dom";
+
+const Landing = () => {
+
+}
+
+export default Landing;
