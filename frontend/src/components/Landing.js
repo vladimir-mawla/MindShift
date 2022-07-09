97917509c9f802bfc4ed9349082e1a8f82dcd04a
vladimir <vladimirmawla2@gmail.com>
2022-07-09 21:04:40 +0300

    Creating return for the landing function

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -5,6 +5,20 @@ import { useNavigate, Link } from "react-router-dom";
 
 const Landing = () => {
 
+
+    return(
+        <div>
+            <Navbar />
+            <div className='hero'>
+                <div className='logo'>
+                    <h1>LOGO Goes Here</h1>
+                </div>
+                <div>
+                    <h1>Form Goes Here</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Landing;
