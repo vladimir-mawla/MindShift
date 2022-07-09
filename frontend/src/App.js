d03cef42e7d3cfa78d89be3d5e12374417082340
vladimir <vladimirmawla2@gmail.com>
2022-07-09 15:02:28 +0300

    Importing login component and creating a route for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+      <Route path="/" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
     );
