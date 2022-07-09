0eb0aac70028ae09038b3453868288787ff01924
vladimir <vladimirmawla2@gmail.com>
2022-07-09 18:01:23 +0300

    Changing the login route in the app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
