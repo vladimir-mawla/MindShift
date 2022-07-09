bccb691f1c7499776ebcabf9e3f229966e9da507
vladimir <vladimirmawla2@gmail.com>
2022-07-09 17:58:03 +0300

    Creating a register function inside the register component

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -6,7 +6,40 @@ import { useRef } from "react";
 import axios from "axios";
 
 const Register = () => {
+  const name = useRef(0);
+  const email = useRef(0);
+  const password = useRef(0);
+  const navigate = useNavigate();
 
+  function register() {
+    if (email.current.value == "" || password.current.value == "" || name.current.value == "") {
+      alert("You should fill all fields");
+    } else {
+      axios
+        .post("http://127.0.0.1:8000/api/v1/register", {
+          name: name.current.value,
+          email: email.current.value,
+          password: password.current.value,
+        })
+
+        .then((res) => {
+          if (res.data["user"]) {
+            localStorage.setItem("user_id", res.data.user.id);
+            navigate("/page");
+          }
+        })
+        .catch((error) => {
+          let message = error.response.data.message;
+          if (message == "The email must be a valid email address.") {
+            alert("The email must be a valid email address.");
+            password.current.value = "";
+          } else if (message == "The password must be at least 6 characters.") {
+            alert("The password must be at least 6 characters.");
+            password.current.value = "";
+          }
+        });
+    }
+  }
 
   return (
     <center>
