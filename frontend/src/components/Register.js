8b02bba758ffe8983c901ab0c1952fa16bd5bf49
vladimir <vladimirmawla2@gmail.com>
2022-07-09 17:53:23 +0300

    Initializing register component

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useNavigate, Link } from "react-router-dom";
+import { useRef } from "react";
+import axios from "axios";
+
+const Register = () => {
+
+};
+export default Register;
