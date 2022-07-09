bef6673c02d2566a29aa31fc9c4abcddb05acd97
vladimir <vladimirmawla2@gmail.com>
2022-07-09 17:59:42 +0300

    Creating a route for the register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Register from './components/Register';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Login />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
     );
