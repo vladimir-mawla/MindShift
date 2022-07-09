bd54d75b8d3191452725f4a48f69ad0d868c2ec9
vladimir <vladimirmawla2@gmail.com>
2022-07-09 14:57:18 +0300

    Importing useref from react router dom

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useRef } from "react-router-dom";
 
 const Login = () => {
 
