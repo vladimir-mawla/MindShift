cad5589e405df74531198b3e7d49f137582db04b
vladimir <vladimirmawla2@gmail.com>
2022-07-09 21:45:46 +0300

    Styling the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <h1>MindShift</h1>
+            <div><h1>MindShift</h1></div>
         </div>
     )
 }
