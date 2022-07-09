6bf31c5cb5e7e090857e3be21ea747bac296dd7f
vladimir <vladimirmawla2@gmail.com>
2022-07-09 20:59:31 +0300

    Estableshing route for the landing component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Landing from './components/Landing';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
       </Routes>
