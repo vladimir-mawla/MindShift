2535b60a6d6139d30ad425b5d7f15d4351b2b864
vladimir <vladimirmawla2@gmail.com>
2022-07-09 16:24:23 +0300

    Using useRef instead of get element by id in the login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,28 +1,30 @@
 import React from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
-import { useNavigate, useRef } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const email = useRef(0);
+  const password = useRef(0);
+  const navigate = useNavigate();
 
     function login() {
-
-        const navigate = useNavigate();
-        const login_email = document.getElementById("login_email");
-        const login_password = document.getElementById("login_password");
-
         axios
           .post("http://127.0.0.1:8000/api/v1/login", {
-            email: login_email.value,
-            password: login_password.value,
+            email: email.current.value,
+            password: password.current.value,
         })
 
         .then((res) => {
+          console.log(res.data)
             if(res.data["message"] == "The email must be a valid email address."){
               alert("The email must be a valid email address.");
-              login_password.value = "";
+              password.current.value = "";
             }else if(res.data['message'] == "Unauthorized"){
                 alert("User Not Found")
+                password.current.value = "";
             }else if (res.data["user"]) {
               localStorage.setItem("user_id", res.data["user"]["_id"]);
               navigate("/page");
@@ -33,11 +35,11 @@ const Login = () => {
   return (
     <center>
       <div className="form">
-        <input type="email" placeholder="email" id="login_email" />
+        <input type="email" placeholder="email" ref={email} />
         <input
           type="password"
           placeholder="password"
-          id="login_password"
+          ref={password}
           autoComplete="on"
         />
         <Button
