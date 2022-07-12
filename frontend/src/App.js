3fc18cf61555e769c51ce2c133c624619e9621d4
vladimir <vladimirmawla2@gmail.com>
2022-07-13 00:51:44 +0300

    Frontend: Creating a route for the games component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Landing from './components/Landing';
+import Games from './components/Games';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Landing />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/games" element={<Games />}></Route>
       </Routes>
     </BrowserRouter>
     );
