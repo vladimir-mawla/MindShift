bd122bdfbb88370d843261ec93fa69ec0f7ed144
vladimir <vladimirmawla2@gmail.com>
2022-07-13 00:49:05 +0300

    Frontend: Creating games component

---
+++ frontend/src/components/Games.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useNavigate, Link } from "react-router-dom";
+import { useRef } from "react";
+import axios from "axios";
+
+const Games = () => {
+  const navigate = useNavigate();
+
+
+  return (
+    <div></div>
+  );
+};
+export default Games;
