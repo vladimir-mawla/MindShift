7322e0d6e8886a5b6c13eaf421ddabb51f525c46
vladimir <vladimirmawla2@gmail.com>
2022-07-13 01:43:43 +0300

    Frontend: Adding the returned data in a card

--- frontend/src/components/Games.js
+++ frontend/src/components/Games.js
@@ -28,8 +28,24 @@ const Games = () => {
             <div className="game">
                 {games.map((game) => (
                     <div className="game" key={game.id}>
-                        {game.name}
-                    </div>
+                <ul class="cards">
+                    <li>
+                        <a href="" class="card">
+                            <img src="https://i.imgur.com/oYiTqum.jpg" class="card-image" alt="" />
+                            <div class="card-overlay">
+                                <div class="card-header">
+                                    <svg class="card-arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+                                    <div class="card-header-text">
+                                        <h3 class="card-title">{game.name}</h3>            
+                                        <span class="card-status">{game.created_at}</span>
+                                    </div>
+                                </div>
+                                <p class="card-description">{game.description}</p>
+                            </div>
+                        </a>      
+                    </li>
+                </ul>
+            </div>
                 ))}
             </div>
         </div>
