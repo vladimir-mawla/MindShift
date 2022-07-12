28aea6bf9142a8cc3b335f0123af5725600d127e
vladimir <vladimirmawla2@gmail.com>
2022-07-13 01:08:43 +0300

    Frontend: Adding axios to the games function

--- frontend/src/components/Games.js
+++ frontend/src/components/Games.js
@@ -7,8 +7,18 @@ import axios from "axios";
 
 const Games = () => {
   const navigate = useNavigate();
+  const [games, setGames] = useState([])
+  
+  axios
+  .get("http://127.0.0.1:8000/api/v1/games/get_games", {
+  })
 
-
+  .then((response) => {
+    const s = response.data.games;
+    console.log(response.data.games)
+    setGames(s);
+  });
+  
   return (
     <div className="container">
         <Navbar />
