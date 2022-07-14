8ac5d517e0b4797bbd67772d0afb1d0d2968fad8
vladimir <vladimirmawla2@gmail.com>
2022-07-15 02:43:46 +0300

    Frontend: Adding axios to get game questions

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -7,7 +7,21 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
 const Game = () => {
+    const [questions, setQuestions] = useState([])
     var game_id = localStorage.getItem('game_id')
+
+    useEffect(() => {
+        axios
+        .get("http://127.0.0.1:8000/api/v1/questions/get_questions", {
+            game_id: game_id
+        })
+    
+        .then((response) => {
+            const s = response.data.questions;
+            setQuestions(s);
+        });
+        }, []);
+        
     return (
         <div>
             <h1>{game_id}</h1>
