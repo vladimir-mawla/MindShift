881c2d347262c169779de313a41130cda58aa188
vladimir <vladimirmawla2@gmail.com>
2022-07-15 03:00:51 +0300

    Frontend: Creating question options component

---
+++ frontend/src/components/QuestionOptions.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const QuestionOptions = (props) => {
+  const [options, setOptions] = useState([]);
+
+
+};
+export default QuestionOptions;
