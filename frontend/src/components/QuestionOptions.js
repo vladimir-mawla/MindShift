4e2bf3842f24f8d01cc8c3e8f5d722c0a8340eeb
vladimir <vladimirmawla2@gmail.com>
2022-07-15 03:02:16 +0300

    Frontend: Using axios to get question options

--- frontend/src/components/QuestionOptions.js
+++ frontend/src/components/QuestionOptions.js
@@ -5,6 +5,14 @@ import axios from "axios";
 const QuestionOptions = (props) => {
   const [options, setOptions] = useState([]);
 
+    axios
+      .post("http://127.0.0.1:8000/api/v1/question_options/get_options", {
+        question_id: props.question_id,
+      })
 
+      .then((response) => {
+        setOptions(response.data["options"]);
+      });
+  
 };
 export default QuestionOptions;
