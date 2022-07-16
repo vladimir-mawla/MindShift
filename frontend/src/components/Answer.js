3ad74bbaedd53ec80be5382f1f470071020e9fe9
vladimir <vladimirmawla2@gmail.com>
2022-07-17 01:56:57 +0300

    Frontend: Adding add answer axios to the answer component

--- frontend/src/components/Answer.js
+++ frontend/src/components/Answer.js
@@ -1,6 +1,19 @@
 import React from "react";
+import axios from "axios";
 
 const Answer = ({props}) => {
+
+    useEffect(() => {
+
+        axios
+        .post("http://127.0.0.1:8000/api/v1/answers/add_answer", {
+            answer: props.answer,
+            question_id: props.question_id,
+            user_id: user_id,
+            game_id: game_id
+        })
+
+    }, []);
 
 }
 
