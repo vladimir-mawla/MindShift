efcfc786952cfae70a71c21fe794d3568112c429
vladimir <vladimirmawla2@gmail.com>
2022-07-17 01:11:09 +0300

    Frontend: Removing the return part from question options component and creating a return for each question type

--- frontend/src/components/QuestionOptions.js
+++ frontend/src/components/QuestionOptions.js
@@ -16,19 +16,25 @@ const QuestionOptions = (props) => {
         });
     }, [props.question_id]);
 
-      
-    return (
-        <div>
-            {options.map((option) => (
-            <div key={option.id}>
-                {question_type === 0 ? <input type='text' />
-                : question_type === 1 ? <div><input type='checkbox' name={props.question_id}/>{option.option}</div>
-                : question_type === 2 ? <div><input type='radio' name={props.question_id}/>{option.option}</div>
-                :""
-                }
-            </div>
-            ))}
-        </div>
-    )
+
+    if(question_type === 0){
+        return(
+         <div>
+
+         </div>   
+        )
+    }else if(question_type === 1){
+        return(
+         <div>
+
+         </div>   
+        )
+    }else if(question_type === 2){
+        return(
+         <div>
+
+         </div>   
+        )
+    }
 };
 export default QuestionOptions;
