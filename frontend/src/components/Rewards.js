c0f106fb25b883719c8be1cc697930b2a350dfff
vladimir <vladimirmawla2@gmail.com>
2022-07-17 18:12:16 +0300

    Frontend: Adding axios request to get rewards

--- frontend/src/components/Rewards.js
+++ frontend/src/components/Rewards.js
@@ -6,6 +6,18 @@ import axios from "axios";
 const Rewards = () => {
     const [rewards, setRewards] = useState([]);
 
+  useEffect(() => {
+
+    axios
+      .get("http://127.0.0.1:8000/api/v1/rewards/get_rewards")
+
+      .then((response) => {
+        const s = response.data.rewards;
+        setRewards(s);
+      });
+
+  }, []);
+
 
 };
 
