c2f30d6fe2793595de2ecec84ca790ab87650b60
vladimir <vladimirmawla2@gmail.com>
2022-07-17 16:22:25 +0300

    Frontend: Creating leaderboard component

---
+++ frontend/src/components/Leaderbord.js
@@ -0,0 +1,18 @@
+import './styles/games.css'
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import QuestionOptions from './QuestionOptions';
+
+const Leaderboard = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+export default Leaderboard;
