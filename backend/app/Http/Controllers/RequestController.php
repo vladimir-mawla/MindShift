0cc190df6374dbb8cc318d749bbec2fc50b9abf2
vladimir <vladimirmawla2@gmail.com>
2022-07-17 19:24:23 +0300

    Backend: Adding add request api

--- backend/app/Http/Controllers/RequestController.php
+++ backend/app/Http/Controllers/RequestController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class RequestController extends Controller
 {
-    //
+    // Add Request API
+    public function addRequest(Request $request){
+        $request = new Request;
+        $request->request = $request->request;
+        $request->reward_id = $request->question_id;
+        $request->user_id = $request->user_id;
+        $request->game_id = $request->game_id;
+        $request->company_id = $request->company_id;
+        $request->save();
+
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
 }
