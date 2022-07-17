9c58d18f575fc1f0c9f4af002aa24ea44d971bbe
vladimir <vladimirmawla2@gmail.com>
2022-07-17 18:10:44 +0300

    Frontend: Creating rewards component

---
+++ frontend/src/components/Rewards.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Navbar from "./Navbar";
+import axios from "axios";
+
+const Rewards = () => {
+    const [rewards, setRewards] = useState([]);
+
+
+};
+
+export default Rewards;
