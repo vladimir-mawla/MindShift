36d09c329bf1480582af7be3b9fe8169c561dcfe
vladimir <vladimirmawla2@gmail.com>
2022-07-17 16:27:12 +0300

    Backend: Creating relationship in the leaderboard model

--- backend/app/Models/Leaderboard.php
+++ backend/app/Models/Leaderboard.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Leaderboard extends Model
 {
     use HasFactory;
+    public function users()
+    {
+        return $this->hasMany(Users::class);
+    }
 }
