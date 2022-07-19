6a811abf1884e51c957b9560302bd08263fdb532
vladimir <vladimirmawla2@gmail.com>
2022-07-20 01:53:09 +0300

    Frontend: Adding axios request in the requests component

--- frontend/src/components/Request.js
+++ frontend/src/components/Request.js
@@ -3,6 +3,16 @@ import axios from "axios";
 
 const Request = ({props}) => {
 
+    useEffect(() => {
+        axios
+        .post("http://127.0.0.1:8000/api/v1/rewards/add_order"), {
+            user_id:blank,
+            reward_id:blank,
+            company_id:blank,
+            
+        }
+    }, [])
+    
 
 }
 
