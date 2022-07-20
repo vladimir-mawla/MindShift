dca8ef7ffe29be663b1bf45e3d100420c6015c8c
vladimir <vladimirmawla2@gmail.com>
2022-07-20 16:00:41 +0300

    Frontend: Creating add game component

---
+++ frontend/src/components/AddGame.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useNavigate, Link } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+
+const AddGame = () => {
+
+  
+
+
+};
+export default AddGame;
