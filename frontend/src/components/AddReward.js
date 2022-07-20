f06028487d174714c7e812fd106ae6a11a07f450
vladimir <vladimirmawla2@gmail.com>
2022-07-21 01:34:43 +0300

    Frontend: Adding return to the add reward component

--- frontend/src/components/AddReward.js
+++ frontend/src/components/AddReward.js
@@ -7,5 +7,14 @@ import axios from "axios";
 
 const AddReward = () => {
 
+    return (
+        <div className="container">
+            <input ref={name} type="text" placeholder="Reward name"></input>
+            <input ref={description} type="text" placeholder="Reward description"></input>
+            <input ref={points} type="text" placeholder="Reward points"></input>
+            <input ref={element} type="file" onChange={() => {encode();}}></input>
+            <Button text={"Submit"} className={"form-btn"} onClick={() => { submit(); }}/>
+        </div>
+    )
 };
 export default AddReward;
