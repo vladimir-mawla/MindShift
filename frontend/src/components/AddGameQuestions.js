45fcb6b039f4abbbdd9394f0cbceef82f0784d5d
vladimir <vladimirmawla2@gmail.com>
2022-07-20 23:52:38 +0300

    Frontend: Adding return to the add game questions component

--- frontend/src/components/AddGameQuestions.js
+++ frontend/src/components/AddGameQuestions.js
@@ -7,5 +7,18 @@ import axios from "axios";
 
 const AddGameQuestion = () => {
 
+    return (
+        <div className="container">
+                    <select name="type" ref={question_type}>
+                        <option value="1">Text</option>
+                        <option value="2">Radio</option>
+                        <option value="3">Checkbox</option>
+                    </select>
+            <input ref={question} type="text" placeholder="Question"></input>
+            <input ref={correct_answer} type="text" placeholder="Correct answer"></input>
+            <input ref={points} type="text" placeholder="Question points"></input>
+            <Button text={"Submit"} className={"form-btn"} onClick={() => { submit(); }}/>
+        </div>
+    )
 };
 export default AddGameQuestion;
