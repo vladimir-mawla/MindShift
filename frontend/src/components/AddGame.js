328ba3a82b62cb01e0e4a812ba6b03dee9174d06
vladimir <vladimirmawla2@gmail.com>
2022-07-20 16:51:17 +0300

    Frontend: Fixing minor issues in the add game component'

--- frontend/src/components/AddGame.js
+++ frontend/src/components/AddGame.js
@@ -7,24 +7,26 @@ import axios from "axios";
 
 const AddGame = () => {
 
-  
-    useEffect(() => {
+  function submit() {
     axios
     .post("http://127.0.0.1:8000/api/v1/games/add_game", {
-
+        name: name.current.value,
+        description: description.current.value,
+        points: points.current.value,
+        img: s,
     })
 
     .then((response) => {
         console.log(response.data.games)
     });
-    }, []);
-
+  }
     return (
         <div className="container">
-            <input type="text" placeholder="Game name"></input>
-            <input type="text" placeholder="Game description"></input>
-            <input type="range" placeholder="Game points"></input>
-            <input type="image"></input>
+            <input ref={name} type="text" placeholder="Game name"></input>
+            <input ref={description} type="text" placeholder="Game description"></input>
+            <input ref={points} type="text" placeholder="Game points"></input>
+            <input ref={element} type="file" onChange={() => {encode();}}></input>
+            <Button text={"Submit"} className={"form-btn"} onClick={() => { submit(); }}/>
         </div>
     )
 };
