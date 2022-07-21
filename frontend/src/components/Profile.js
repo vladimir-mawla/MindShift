1ef63fd829ebd2cb97cc91a2c8c4d4d204f85970
vladimir <vladimirmawla2@gmail.com>
2022-07-21 20:23:13 +0300

    Frontend: Adding axios to get user's info

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -7,5 +7,16 @@ import axios from "axios";
 
 const Profile = () => {
     
+    useEffect(() => {
+        axios
+        .post("http://127.0.0.1:8000/api/v1/users/get_user", {
+            user_id: localStorage.getItem('user_id')
+        })
+    
+        .then((response) => {
+            console.log(response.data)
+        });
+        }, []);
+
 };
 export default Profile;
