0d57ccd119b236c3e1c2efa04d6b67f0f096a073
vladimir <vladimirmawla2@gmail.com>
2022-07-22 00:49:43 +0300

    Frontend: Displaying user's info

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const Profile = () => {
     const [info, setInfo] = useState([])
-    
+
     useEffect(() => {
         axios
         .post("http://127.0.0.1:8000/api/v1/users/get_user", {
@@ -15,11 +15,21 @@ const Profile = () => {
         })
     
         .then((response) => {
-            console.log(response.data)
-            const s = response.data.users
+            console.log(response.data.users[0])
+            const s = response.data.users[0]
             setInfo(s)
         });
         }, []);
+
+        return (
+            <div>
+                <img src={info.profile_img} />
+                <h1>{info.name}</h1>
+                <h2>points: {info.points}</h2>
+                <h2>email: {info.email}</h2>
+                <h2>job title: {info.job_title}</h2>
+            </div>
+        )
 
 };
 export default Profile;
