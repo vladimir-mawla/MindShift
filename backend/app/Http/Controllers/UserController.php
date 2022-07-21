38dbaf23d593f037398ae2091a24536e637b1e43
vladimir <vladimirmawla2@gmail.com>
2022-07-21 14:38:54 +0300

    Backend: Creating get users api

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    //Get Company Users
+    public function getUsers(Request $request){
+        $users = User::all()->where('company_id', $request->company_id);
+
+        return response()->json([
+            "status" => "success",
+            "users" => $users,
+        ], 200);
+    }
 }
