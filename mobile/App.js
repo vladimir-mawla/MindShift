2c9e294baa2e85a5fb05047598bdbb9ae5c38147
vladimir <vladimirmawla2@gmail.com>
2022-07-22 17:04:09 +0300

    Mobile: Creating Login component

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Landing from "./components/Landing";
+import Login from "./components/Login";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Landing />
+      <Login />
       <StatusBar style="auto" />
     </View>
   );
---
+++ mobile/components/Login.js
@@ -0,0 +1,10 @@
+import { StyleSheet, View, Text, TouchableOpacity, Alert, Image, Button } from "react-native";
+
+export default function Login() {
+  return (
+    <View>
+        <Text>Hello</Text>
+    </View>
+  );
+}
+
