19f4238eabbc94e521bc1fc35228bc996443e9f8
vladimir <vladimirmawla2@gmail.com>
2022-07-22 15:36:40 +0300

    Mobile: Adding logo to the landing page

--- mobile/components/Landing/Landing.js
+++ mobile/components/Landing/Landing.js
@@ -1,10 +1,9 @@
-import { View, Text, TouchableOpacity, Alert} from 'react-native';
+import { View, Text, TouchableOpacity, Alert, Image } from "react-native";
 
 export default function Landing() {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}><Text>Login</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}><Text>Register</Text></TouchableOpacity>
-        </View>
-    )
+  return (
+    <View>
+      <Image source={require("./logo.jpeg")} />
+    </View>
+  );
 }
