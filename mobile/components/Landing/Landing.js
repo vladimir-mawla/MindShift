95d75c6aaaa45e99452ac8ff066cd55389195953
vladimir <vladimirmawla2@gmail.com>
2022-07-22 14:51:08 +0300

    Mobile: Initializing landing function

---
+++ mobile/components/Landing/Landing.js
@@ -0,0 +1,11 @@
+import { View, Text} from 'react-native';
+
+export default function Landing() {
+    return (
+        <View>
+            <Text>
+                Hello
+            </Text>
+        </View>
+    )
+}
