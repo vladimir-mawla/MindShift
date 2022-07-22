862c32d925a3bd236f1598f032a9cc28def62326
vladimir <vladimirmawla2@gmail.com>
2022-07-22 17:14:30 +0300

    Mobile: Styling the text input in the login component

--- mobile/components/Login.js
+++ mobile/components/Login.js
@@ -3,14 +3,16 @@ import { StyleSheet, View, Text, TouchableOpacity, Alert, Image, Button, TextInp
 export default function Login() {
   return (
     <View>
-        <View>
+        <View style={styles.inputView}>
             <TextInput 
+                style={styles.textInput}
                 placeholder="Email"
                 placeholderTextColor="#003f5c"
             />
         </View>
-        <View>
+        <View style={styles.inputView}>
             <TextInput 
+                style={styles.textInput}
                 placeholder="Password"
                 placeholderTextColor="#003f5c"
                 secureTextEntry={true}
@@ -20,3 +22,22 @@ export default function Login() {
   );
 }
 
+
+const styles = StyleSheet.create({
+    inputView: {
+        backgroundColor: "#95BDCE",
+        borderRadius: 30,
+        width: 200,
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+      },
+      
+      textInput: {
+        width:"100%",
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+      }
+  });
