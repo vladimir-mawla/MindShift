484d508e1ce8646d20095bbca1c1ca2032118f65
vladimir <vladimirmawla2@gmail.com>
2022-07-22 17:10:12 +0300

    Mobile: Adding text inputs to the login component

--- mobile/components/Login.js
+++ mobile/components/Login.js
@@ -1,9 +1,21 @@
-import { StyleSheet, View, Text, TouchableOpacity, Alert, Image, Button } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Alert, Image, Button, TextInput } from "react-native";
 
 export default function Login() {
   return (
     <View>
-        <Text>Hello</Text>
+        <View>
+            <TextInput 
+                placeholder="Email"
+                placeholderTextColor="#003f5c"
+            />
+        </View>
+        <View>
+            <TextInput 
+                placeholder="Password"
+                placeholderTextColor="#003f5c"
+                secureTextEntry={true}
+            />
+        </View>
     </View>
   );
 }
