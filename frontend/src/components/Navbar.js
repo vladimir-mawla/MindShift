e797c474ff5d281c2f8de5c3fdce194a2242db84
vladimir <vladimirmawla2@gmail.com>
2022-07-23 01:00:27 +0300

    Frontend: Adding links to the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,6 +9,8 @@ const Navbar = () => {
             <ul className='nav-links'>
                 <li><Link to={"/leaderboards"}>Leaderboard</Link></li>
                 <li><Link to={"/rewards"}>Rewards</Link></li>
+                <li><Link to={"/profile"}>Profile</Link></li>
+                <li><Link to={"/games"}>Games</Link></li>
             </ul>
             
         </nav>
