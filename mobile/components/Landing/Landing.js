0e02d52a0948221b00c366e901498ec1ceaf5629
vladimir <vladimirmawla2@gmail.com>
2022-07-22 15:41:48 +0300

    Mobile: Styling logo in the landing component

--- mobile/components/Landing/Landing.js
+++ mobile/components/Landing/Landing.js
@@ -2,8 +2,11 @@ import { View, Text, TouchableOpacity, Alert, Image } from "react-native";
 
 export default function Landing() {
   return (
-    <View>
-      <Image source={require("./logo.jpeg")} />
+    <View style={{ flex: 1 }}>
+      <Image
+        style={{ height: 200, width: 200, marginTop: 100 }}
+        source={require("./logo.jpeg")}
+      />
     </View>
   );
 }
