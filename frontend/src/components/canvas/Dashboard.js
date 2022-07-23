3073bf9ff5f9ff580526f04abfca70e7576f6053
vladimir <vladimirmawla2@gmail.com>
2022-07-23 19:13:36 +0300

    Frontend: Fixing chart in the dashboard component

--- frontend/src/components/canvas/Dashboard.js
+++ frontend/src/components/canvas/Dashboard.js
@@ -1,41 +1,61 @@
 import React, { Component } from 'react';
 import CanvasJSReact from './canvasjs.react';
+import axios from 'axios';
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
  
 class Dashboard extends Component {	
+	constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
 
-	
+        };
 
+    }
+	componentDidMount() {
+		axios
+        .post("http://127.0.0.1:8000/api/v1/users/get_users", {
+            company_id: localStorage.getItem("company_id")
+        })
+        .then((response) => {
+            const s = response.data.users;
+            // this.state.users(s);
+			this.setState({ users: [...this.state.users, s] })
+            
+        });
+	  }
 	render() {
+		// console.log(this.state.users)
+		const results =[];
+		this.state.users.map((user) =>(
+			user.forEach(u => {
+				results.push(
+					{ y:u.points, label:u.name }
+				)})
+		))
 		const options = {
 			animationEnabled: true,
 			exportEnabled: true,
 			theme: "dark2", // "light1", "dark1", "dark2"
 			title:{
-				text: "Trip Expenses"
+				text: "Employees Progress"
 			},
 			data: [{
 				type: "pie",
 				indexLabel: "{label}: {y}%",		
 				startAngle: -90,
-				dataPoints: [
-					{ y: 20, label: "Airfare" },
-					{ y: 24, label: "Food & Drinks" },
-					{ y: 20, label: "Accomodation" },
-					{ y: 14, label: "Transportation" },
-					{ y: 12, label: "Activities" },
-					{ y: 10, label: "Misc" }	
-				]
+				dataPoints: results
+					
+	
+				
 			}]
 		}
 		
 		return (
 		<div>
-			<CanvasJSChart options = {options} 
-				/* onRef={ref => this.chart = ref} */
+			<CanvasJSChart options = {options}
 			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
 		</div>
 		);
 	}
