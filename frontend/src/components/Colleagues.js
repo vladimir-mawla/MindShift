da56bc806b827a4f76e028cd5cd8af9cd9275703
vladimir <vladimirmawla2@gmail.com>
2022-07-23 10:29:21 +0300

    Frontend: Adding axios to get colleagues

--- frontend/src/components/Colleagues.js
+++ frontend/src/components/Colleagues.js
@@ -7,6 +7,21 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
 const Colleagues = () => {
+    const navigate = useNavigate();
+    const [colleagues, setColleagues] = useState([])
+  
+    useEffect(() => {
+    axios
+    .post("http://127.0.0.1:8000/api/v1/gained_rewards/get_colleagues", {
+        company_id: localStorage.getItem('company_id')
+    })
+
+    .then((response) => {
+        const s = response.data.users;
+        console.log(response.data.users)
+        setColleagues(s);
+    });
+    }, []);
 
   
     return (
