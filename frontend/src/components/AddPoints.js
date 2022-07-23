40bfe77a411f3f1df2679a682b4f2bae10e25680
vladimir <vladimirmawla2@gmail.com>
2022-07-23 13:45:17 +0300

    Frontend: Adding axios request to get employees in the add points component

--- frontend/src/components/AddPoints.js
+++ frontend/src/components/AddPoints.js
@@ -6,6 +6,19 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
 const AddPoints = () => {
+    const [employees, setEmployees] = useState([])
+
+    function increment() {
+        axios
+        .post("http://127.0.0.1:8000/api/v1/users/get_users", {
+            company_id: localStorage.getItem("company_id")
+        })
+        .then((response) => {
+            const s = response.data.users;
+            setEmployees(s);
+        });
+
+    }
 
     return (
         <>
