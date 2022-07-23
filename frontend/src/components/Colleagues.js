c32eaf259e168f0c9654dcebc5f123b7f00d5daf
vladimir <vladimirmawla2@gmail.com>
2022-07-23 10:28:04 +0300

    Frontend: Creating colleagues component

---
+++ frontend/src/components/Colleagues.js
@@ -0,0 +1,16 @@
+import './styles/games.css';
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useNavigate, Link } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+
+const Colleagues = () => {
+
+  
+    return (
+        <div></div>
+    );
+};
+export default Colleagues;
