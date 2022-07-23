1038c0c0529a8d2957f5f5980a9d78f1196c78dc
vladimir <vladimirmawla2@gmail.com>
2022-07-23 15:22:44 +0300

    Frontend: Adding axios to get employees and badges

--- frontend/src/components/AddBadge.js
+++ frontend/src/components/AddBadge.js
@@ -6,7 +6,45 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
 const AddBadge = () => {
+    const user = useRef(null)
+    const badge = useRef(null)
+    const [employees, setEmployees] = useState([])
+    const [badges, setBadges] = useState([])
+        
+    useEffect(() => {
+        axios
+        .post("http://127.0.0.1:8000/api/v1/users/get_users", {
+            company_id: localStorage.getItem("company_id")
+        })
+        .then((response) => {
+            const s = response.data.users;
+            setEmployees(s);
+            
+        });
+    }, [])
+    useEffect(() => {
+        axios
+        .get("http://127.0.0.1:8000/api/v1/badges/get_badges")
 
+        .then((response) => {
+            const s = response.data.badges;
+            setBadges(s);
+            
+        });
+    }, [])
+    
+    function addBadge(){
+        axios
+        .post("http://127.0.0.1:8000/api/v1/users/add_badge", {
+            user_id: user.current.value,
+            badge: badge.current.value,
+        })
+        .then((response)=> {
+            alert('Assigned badge to the selected employee')
+            user.current.value=""
+            badge.current.value=""
+        })
+    }
     
 
     return (
