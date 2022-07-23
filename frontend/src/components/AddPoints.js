0d461e243ca1745b950edaf912c4a8d2e4f5d0f2
vladimir <vladimirmawla2@gmail.com>
2022-07-23 14:09:06 +0300

    Frontend: Adding a return for the add points component

--- frontend/src/components/AddPoints.js
+++ frontend/src/components/AddPoints.js
@@ -7,21 +7,31 @@ import axios from "axios";
 
 const AddPoints = () => {
     const [employees, setEmployees] = useState([])
-
-    function increment() {
+        
+    useEffect(() => {
         axios
         .post("http://127.0.0.1:8000/api/v1/users/get_users", {
             company_id: localStorage.getItem("company_id")
         })
         .then((response) => {
             const s = response.data.users;
+            console.log(s)
             setEmployees(s);
         });
-
-    }
+    }, [])
+    
 
     return (
         <>
+        <label htmlFor="empolyees">Choose an employee:</label>
+        <select name="employees">
+            {employees.map((employee, index) => (
+                <option key={index} value={employee.id}>{employee.name}</option>
+            ))}
+        </select><br/>
+        <label>Assign points:</label>
+        <input type="text"/><br/>
+        <Button text={"Assign"}/>
         </>
     )
 };
