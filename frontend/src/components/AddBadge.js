9d21f31649687a0fbeb365945fc35a04760edf8f
vladimir <vladimirmawla2@gmail.com>
2022-07-23 15:21:53 +0300

    Frontend: Adding a return for the add badge component

--- frontend/src/components/AddBadge.js
+++ frontend/src/components/AddBadge.js
@@ -7,5 +7,24 @@ import axios from "axios";
 
 const AddBadge = () => {
 
+    
+
+    return (
+        <>
+        <label htmlFor="empolyees">Choose an employee:</label>
+        <select ref={user} name="employees">
+            {employees.map((employee, index) => (
+                <option key={index} value={employee.id}>{employee.name}</option>
+            ))}
+        </select><br/>
+        <label htmlFor="badges">Choose an employee:</label>
+        <select ref={badge} name="badges">
+            {badges.map((badge, index) => (
+                <option key={index} value={badge.badge}>{badge.name}</option>
+            ))}
+        </select><br/>
+        <Button text={"Assign"} onClick={addBadge}/>
+        </>
+    )
 };
 export default AddBadge;
