462e509fc344ced0abbd134c89b2bc8681226e68
vladimir <vladimirmawla2@gmail.com>
2022-07-23 14:29:21 +0300

    Frontend: Finalizing add points component

--- frontend/src/components/AddPoints.js
+++ frontend/src/components/AddPoints.js
@@ -6,7 +6,7 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
 const AddPoints = () => {
-    const employee = useRef(null)
+    const user = useRef(null)
     const points = useRef(null)
     const [employees, setEmployees] = useState([])
         
@@ -17,16 +17,21 @@ const AddPoints = () => {
         })
         .then((response) => {
             const s = response.data.users;
-            console.log(s)
             setEmployees(s);
+            
         });
     }, [])
-
+    
     function incremenet(){
         axios
-        .post("http://127.0.0.1:8000/api/v1/users/get_users", {
-            user_id: employee.current.value,
+        .post("http://127.0.0.1:8000/api/v1/users/points_control", {
+            user_id: user.current.value,
             points: points.current.value,
+        })
+        .then((response)=> {
+            alert(`Assigned ${points.current.value} points to the selected user`)
+            user.current.value=""
+            points.current.value=""
         })
     }
     
@@ -34,14 +39,14 @@ const AddPoints = () => {
     return (
         <>
         <label htmlFor="empolyees">Choose an employee:</label>
-        <select name="employees">
+        <select ref={user} name="employees" placeholder="select">
             {employees.map((employee, index) => (
-                <option ref={employee} key={index} value={employee.id}>{employee.name}</option>
+                <option key={index} value={employee.id}>{employee.name}</option>
             ))}
         </select><br/>
         <label>Assign points:</label>
         <input ref={points} type="text"/><br/>
-        <Button text={"Assign"}/>
+        <Button text={"Assign"} onClick={incremenet}/>
         </>
     )
 };
