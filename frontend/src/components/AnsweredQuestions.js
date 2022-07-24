2208df1079d7f87dcb4fdcc270757c2c4a65aba8
vladimir <vladimirmawla2@gmail.com>
2022-07-24 13:28:14 +0300

    Frontend: Creating answered questions component

---
+++ frontend/src/components/AnsweredQuestions.js
@@ -0,0 +1,44 @@
+import './styles/games.css'
+import React from "react";
+import Button from "./Button";
+import Navbar from "./Navbar";
+import { useNavigate, Link, Navigate } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import QuestionOptions from './QuestionOptions';
+
+const Questions = () => {
+    const navigate = useNavigate();
+    const [questions, setQuestions] = useState([])
+    var game_id = localStorage.getItem('game_id')
+
+    useEffect(() => {
+        axios
+        .post("http://127.0.0.1:8000/api/v1/questions/get_questions", {
+            game_id: game_id
+        })
+    
+        .then((response) => {
+            const s = response.data.questions;
+            console.log(response.data.questions);
+            setQuestions(s);
+        });
+        }, []);
+        
+    return (
+        <div>
+            <ul>
+                {questions.map((question) => (
+                    <div key={question.id}>
+                        <li key={question.id}>{question.question} ({question.points} points)</li>
+                        {(question.question_type === 0) ? <div><input onChange={(e) => setName(e.target.value)} id={question.id} type={"text"} /></div> : 
+                        <QuestionOptions setName={setName} name={name} question_id={question.id} question_type={question.question_type}/>
+                        }
+                    </div>
+                ))}
+            </ul>
+            
+        </div>
+    );
+};
+export default Questions;
