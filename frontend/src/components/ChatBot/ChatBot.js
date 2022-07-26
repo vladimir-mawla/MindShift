85a58c82634f64fb06b3517d090184f48cc5e248
vladimir <vladimirmawla2@gmail.com>
2022-07-27 01:15:47 +0300

    Frontend: Adding function to get the chatbot

--- frontend/src/components/ChatBot/ChatBot.js
+++ frontend/src/components/ChatBot/ChatBot.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react';
 
 class Chatbot extends Component {
 
-
+  componentDidMount() {
+    (function(d, m){
+      var kommunicateSettings = {"appId":"34f63a33d09b5665fc39b585e12d27dd0","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }
 
   render(){
     return(
