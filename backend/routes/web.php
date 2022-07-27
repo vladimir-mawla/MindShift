3d4d5a4cd40a9d77e5adb97e9caafe16df8f6214
vladimir <vladimirmawla2@gmail.com>
2022-07-27 16:47:44 +0300

    Backend: Creating a broadcast channel

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/abc', function () {
-    event(new App\Events\MyEvent('hey'));
-    return "sent";
-});
