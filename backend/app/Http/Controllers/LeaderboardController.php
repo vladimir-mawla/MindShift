bab52416b3e39e00af48e4061bc13847c76137ae
vladimir <vladimirmawla2@gmail.com>
2022-07-27 19:20:46 +0300

    Frontend: Creating get leaderboards points api

--- backend/app/Http/Controllers/LeaderboardController.php
+++ backend/app/Http/Controllers/LeaderboardController.php
@@ -20,12 +20,19 @@ class LeaderboardController extends Controller
         ], 200);
     }
     // Get Leaderboard API
-    public function getLeaderboard(){
-        $users = User::all()->take(5);
+    public function getLeaderboard(Request $request){
+        $company_id = $request->company_id;
+        $users = User::all()->where("company_id", $company_id)->take(5);
 
         return response()->json([
             "status" => "success",
             "users" => $users,
         ], 200);
+    }
+    // Get Leaderboard's points API
+    public function pushLeader(){
+        $leaders = User::all();
+        $leaders->makeHidden(['email', 'badge', 'profile_img', "country", "city", "description", "email_verified_at", "user_type","job_title", "created_at","updated_at", "level", "id", "company_id"]);
+        event(new MyEvent([$leaders]));
     }
 }
