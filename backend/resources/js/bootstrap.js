c98c891e4907c1ee9bf281564f8cc7605bb49db1
vladimir <vladimirmawla2@gmail.com>
2022-07-27 16:47:04 +0300

    Backend: Changing bootstrap.js file

--- backend/resources/js/bootstrap.js
+++ backend/resources/js/bootstrap.js
@@ -1,34 +1,22 @@
-import _ from 'lodash';
-window._ = _;
+window._ = require('lodash');
+    
+window.axios = require('axios');
+window.moment = require('moment')
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-import axios from 'axios';
-window.axios = axios;
+// import 'vue-tel-input/dist/vue-tel-input.css';
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+window.axios.defaults.headers.common.crossDomain = true;
+window.axios.defaults.baseURL = '/api';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+let token = document.head.querySelector('meta[name="csrf-token"]');
 
-// import Echo from 'laravel-echo';
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://adonisjs.com/docs/4.1/csrf');
+}
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Pusher = require('pusher-js');
