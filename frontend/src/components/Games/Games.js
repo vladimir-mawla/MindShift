3a6c75eefb74893c2ba29d5f1a51add1a62d86f9
vladimir <vladimirmawla2@gmail.com>
2022-07-29 12:15:24 +0300

    Frontend: Styling available games in the page component

--- frontend/src/components/Games/Games.js
+++ frontend/src/components/Games/Games.js
@@ -35,36 +35,43 @@ const Games = () => {
 
   
     return (
-        <div className="container">
-            <Navbar />
-            <ChatBot />
-            <div className="game">
+        <>
                 {games.map((game) => (
-                <div key={game.id}>
-                <ul className="cards">
-                    <li id={game.id} onClick={handleClick}>
-                        <a href="" className="card">
-                            <img src={game.img} className="card-image" alt="" />
-                            <div className="card-overlay">
-                                <div className="card-header">
-                                    <svg className="card-arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-                                    <div className="card-header-text">
-                                        <div className='dis'>
-                                            <div className='name'><h3 className="card-title">{game.name}</h3></div>
-                                            <div className='points'><h3>{game.points}</h3></div>
-                                        </div>            
-                                        <span className="card-status">{game.created_at.slice(5,10)} at {game.created_at.slice(11,16)}</span>
-                                    </div>
-                                </div>
-                                <p className="card-description">{game.description}</p>
-                            </div>
-                        </a>      
-                    </li>
-                </ul>
-            </div>
+                    <div className='game-card' key={game.id} onClick={handleClick}>
+                        <div className='game-info'>
+                            <h2>OFFICE QUIZ #{game.id}</h2>
+                            <p>Added {game.created_at.slice(8,10)} {game.created_at.slice(5,7)} at {game.created_at.slice(11,16)}</p>
+                        </div>
+                        <div className='game-contents'>
+                            <h4>100 Questions</h4>
+                            <div className='game-vl'></div>
+                            <div className='points'><h4>Up to 100 Points </h4> <div id='coins'></div></div>
+                        </div>
+                    </div>
+            //     <div key={game.id}>
+            //     <ul className="cards">
+            //         <li id={game.id} onClick={handleClick}>
+            //             <a href="" className="card">
+            //                 <img src={game.img} className="card-image" alt="" />
+            //                 <div className="card-overlay">
+            //                     <div className="card-header">
+            //                         <svg className="card-arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+            //                         <div className="card-header-text">
+            //                             <div className='dis'>
+            //                                 <div className='name'><h3 className="card-title">{game.name}</h3></div>
+            //                                 <div className='points'><h3>{game.points}</h3></div>
+            //                             </div>            
+            //                             <span className="card-status">{game.created_at.slice(5,10)} at {game.created_at.slice(11,16)}</span>
+            //                         </div>
+            //                     </div>
+            //                     <p className="card-description">{game.description}</p>
+            //                 </div>
+            //             </a>      
+            //         </li>
+            //     </ul>
+            // </div>
                 ))}
-            </div>
-        </div>
+        </>
     );
 };
 export default Games;
--- frontend/src/components/Page/Page.js
+++ frontend/src/components/Page/Page.js
@@ -1,13 +1,35 @@
+import './pages.css';
 import React from 'react';
 import ChatBot from '../ChatBot/ChatBot';
 import Navbar from '../Navbar/Navbar';
+import Games from '../Games/Games';
 
 const Page = () => {
 
 return (
     <>
         <Navbar />
-        <ChatBot />
+        <div className='container'>
+            <div className='title'><h1>AVAILABLE GAMES</h1></div>
+            <div className='main-body'>
+                <div className='games-section'>
+                    <Games />
+                </div>
+                <div className='users-section'>
+                    <h1>TOP ACTIVE</h1>
+
+                    <div className='top-users'>
+                        <h1>user</h1>
+                    </div>
+                    <h1>COLLEAGUES</h1>
+
+                    <div className='col-users'>
+                        <h1>col</h1>
+                    </div>
+                </div>
+                {/* <ChatBot /> */}
+            </div>
+        </div>
     </>
 )
 }
