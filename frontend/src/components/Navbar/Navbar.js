64d7884879a089209964e3c1fe59dcaaa67cb70a
vladimir <vladimirmawla2@gmail.com>
2022-07-29 10:28:07 +0300

    Frontend: Styling the navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,22 +1,47 @@
 import './navbar.css'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import { Link, useNavigate, useMatch, useResolvedPath } from 'react-router-dom';
 
 export default function Navbar() {
+  const [info, setInfo] = useState([])
+
+  useEffect(() => {
+    axios
+    .post("http://127.0.0.1:8000/api/v1/users/get_user", {
+        user_id: localStorage.getItem('user_id'),
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+            Accept: 'application/json'
+        }
+    })
+
+    .then((response) => {
+        console.log(response.data.users[0])
+        const s = response.data.users[0]
+        setInfo(s)
+    });
+    }, []);
 
     const navigate = useNavigate()
     const handleClick = (event) => {
         navigate("/page");
     }
     return (
-        <nav className='navbar'>
-            <img id='nav-logo' src={ require('../assets/logo.png') } onClick={handleClick}/>
-            <ul className='nav-links'>
-                <CustomLink to={"/leaderboards"}>Leaderboard</CustomLink>
-                <CustomLink to={"/rewards"}>Rewards</CustomLink>
-                <CustomLink to={"/profile"}>Profile</CustomLink>
-                <CustomLink to={"/games"}>Games</CustomLink>
-                <CustomLink to={"/c"}>Colleagues</CustomLink>
+        <nav className='user-navbar'>
+              <img id='nav-logo' src={ require('../assets/logo.png') } onClick={handleClick}/>
+            <ul className='user-nav-links'>
+                <div className='links'><CustomLink to={"/rewards"}>Rewards</CustomLink></div>
+                <div className="vl"></div>
+                <div className='user-profile-nav'>
+                      <div className='user-profile-info'>
+                        <h5>{info.name}</h5><br/>
+                        <div id='points'><p>{info.points} Points</p><div id='coins'></div></div>
+                      </div>
+                      <div className='user-profile-img'>
+                        <img src={info.profile_img} id='profile-img'/>
+                      </div>
+                </div>
             </ul>
             
         </nav>
