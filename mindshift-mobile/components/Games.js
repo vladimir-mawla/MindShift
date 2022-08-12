f1bf38b4dc656b3a7c49f9ebf16eb2fb5385cc7e
vladimir <vladimirmawla2@gmail.com>
2022-08-13 02:11:33 +0300

    Mobile: Changing game container from view to touchable oppacity in the games component

--- mindshift-mobile/components/Games.js
+++ mindshift-mobile/components/Games.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect }  from "react";
 import axios from "axios";
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Games = () => {
@@ -8,7 +8,7 @@ const Games = () => {
     const [token, setToken] = useState("");
     const getGamesUrl = "http://127.0.0.1:8000/api/v1/games/get_games";
     
-    // saving the game id in local storage
+    // saving the game id in async storage
     const handleClick = (event) => {
         AsyncStorage.setItem('game_id', event.currentTarget.id);
         // navigate("/questions");
@@ -44,7 +44,7 @@ const Games = () => {
     return (
         <View>
                 {games.map((game) => (
-                    <View className='game-card' id={game.id} key={game.id} onClick={handleClick}>
+                    <TouchableOpacity className='game-card' nativeID={game.id} key={game.id} onPress={handleClick}>
                         <View className='game-info'>
                             <h2>OFFICE QUIZ #{game.id}</h2>
                             <p>Added {game.created_at.slice(8,10)} {game.created_at.slice(5,7)} at {game.created_at.slice(11,16)}</p>
@@ -54,7 +54,7 @@ const Games = () => {
                             <View className='game-vl'></View>
                             <View className='points'><h4>Up to {game.points} Points </h4> <View className='game-coins'></View></View>
                         </View>
-                    </View>
+                    </TouchableOpacity>
                 ))}
         </View>
     );
