1f8858715c5af7095aed894c396008fcb49a7b4a
vladimir <vladimirmawla2@gmail.com>
2022-08-17 19:44:31 +0300

    Mobile: Creating rewards component

---
+++ mindshift-mobile/components/Rewards.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect }  from "react";
+import axios from "axios";
+import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Rewards = () => {
+
+};
+
+export default Rewards;
