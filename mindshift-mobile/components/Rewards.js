aefa8ef1bb602b75c66ffa073934e42c051bd4a6
vladimir <vladimirmawla2@gmail.com>
2022-08-17 19:45:06 +0300

    Mobile: Defining constants to be used in the rewards component

--- mindshift-mobile/components/Rewards.js
+++ mindshift-mobile/components/Rewards.js
@@ -4,6 +4,15 @@ import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Rewards = () => {
+    const navigate = useNavigate();
+    const [rewards, setRewards] = useState([]);
+    const [token, setToken] = useState("");
+    const [userId, setUserId] = useState("");
+    const [rewardId, setRewardId] = useState("");
+    const pointsControlUrl = "http://127.0.0.1:8000/api/v1/users/points_control";
+    const addGainedRewardUrl = "http://127.0.0.1:8000/api/v1/gained_rewards/add_gained_reward";
+    const getRewardsUrl = "http://127.0.0.1:8000/api/v1/rewards/get_rewards";
+
 
 };
 
