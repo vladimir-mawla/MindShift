9768cdc8dd29e45648d14a87338fd717b884daa4
vladimir <vladimirmawla2@gmail.com>
2022-10-12 22:27:29 +0300

    Backend: Fixing routes in models

--- mindshift-backend/app/Domain/Rewards/Reward.php
+++ mindshift-backend/app/Domain/Rewards/Reward.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Domain\Rewards;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
--- mindshift-backend/app/Domain/Users/User.php
+++ mindshift-backend/app/Domain/Users/User.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Models;
+namespace App\Domain\Users;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
